b3cad4fd8f46d31aef516e4a0b41cc98d5fe690c
medepallipremchandu <premchandumedepalli@gmail.com>
2023-12-27 14:29:48 +0530

    portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,40 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import "./Navbar.css"
+import { Link, useLocation } from 'react-router-dom';
+import "./Navbar.css";
 
 function Public() {
   const [expanded, setExpanded] = React.useState(false);
+  const location = useLocation();
 
   const handleToggle = () => setExpanded(!expanded);
 
-  const navigateTo = (path) => {
-    setExpanded(false); // Close the navbar before navigating
-    window.location.href = path;
-  };
+  const closeNavbar = () => setExpanded(false);
 
   return (
-    <>
-      <Navbar bg="dark" variant="dark" expand="lg" fixed="top" expanded={expanded}>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
-        <Navbar.Collapse id="basic-navbar-nav ">
-          <Nav className="gradient-underline mr-auto">
-            <Nav.Link onClick={() => navigateTo('/Home')}>HOME</Nav.Link>
-            <Nav.Link onClick={() => navigateTo('/About')}>ABOUT</Nav.Link>
-            <Nav.Link onClick={() => navigateTo('/Skills')}>SKILLS</Nav.Link>
-            <Nav.Link onClick={() => navigateTo('/Highlights')}>HIGHLIGHTS</Nav.Link>
-            <Nav.Link onClick={() => navigateTo('/Portfolio')}>PORTFOLIO</Nav.Link>            
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top" expanded={expanded}>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
+      <Navbar.Collapse id="basic-navbar-nav" onClick={closeNavbar}>
+        <Nav className="mr-auto gradient-underline">
+          <Nav.Link as={Link} to="/Home" className={location.pathname === '/Home' ? 'active' : ''}>
+            HOME
+          </Nav.Link>
+          <Nav.Link as={Link} to="/About" className={location.pathname === '/About' ? 'active' : ''}>
+            ABOUT
+          </Nav.Link>
+          <Nav.Link as={Link} to="/Skills" className={location.pathname === '/Skills' ? 'active' : ''}>
+            SKILLS
+          </Nav.Link>
+          <Nav.Link as={Link} to="/Highlights" className={location.pathname === '/Highlights' ? 'active' : ''}>
+            HIGHLIGHTS
+          </Nav.Link>
+          <Nav.Link as={Link} to="/Portfolio" className={location.pathname === '/Portfolio' ? 'active' : ''}>
+            PORTFOLIO
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
